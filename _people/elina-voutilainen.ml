1cbb3235cbe5f210cbc6189edc271a899f119dd1
Jarno Laitinen <jarno.laitinen@csc.fi>
2022-11-29 09:37:32 +0200

    Create elina-voutilainen.ml

---
+++ _people/elina-voutilainen.ml
@@ -0,0 +1,14 @@
+---
+layout: master
+include: person
+name: Elina Voutilainen
+home: <a href="https://www.csc.fi">CSC - IT Centere for Science Ltd.</a>
+country: "FI"
+photo:
+email: elina.voutilainen@csc.fi
+phone:
+on_contract: yes
+has_been_on_contract: no
+groups:
+  puhuri:
+---
